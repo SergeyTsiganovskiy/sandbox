738e0ab1bb63f654c4b9ba3e36530708b61667f8
Sergey <you@example.comtsyganovskiyss@gmail.com>
2018-09-23 01:49:54 +0300

    Initial commit

---
+++ sandbox/sandbox/Program_1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sandbox
+{
+    public class Program_1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
