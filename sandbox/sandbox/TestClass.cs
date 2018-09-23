3675de77cb2e603805022b0c95c005125bb0caa4
Sergey <you@example.comtsyganovskiyss@gmail.com>
2018-09-23 11:08:30 +0300

    test commit 8

---
+++ sandbox/sandbox/TestClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using NUnit.Framework;
+
+namespace sandbox
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void Method1()
+        {
+            Assert.IsTrue(1 > 0);
+        }
+
+        [Test]
+        public void Method2()
+        {
+            Assert.IsTrue(1 > 0);
+        }
+
+        [Test]
+        public void Method3()
+        {
+            Assert.IsTrue(1 < 0);
+        }
+
+        [Test]
+        public void Method4()
+        {
+            Assert.IsTrue(1 < 0);
+        }
+
+        [Test]
+        public void Method5()
+        {
+            Assert.IsTrue(1 > 0);
+        }
+    }
+}
